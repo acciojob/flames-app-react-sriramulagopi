d5474eed9e4a9c5286cc92453b60995458411db9
sriramulagopi <129825285+sriramulagopi@users.noreply.github.com>
2024-09-17 08:01:12 +0000

    new code

--- src/components/App.js
+++ src/components/App.js
@@ -1,88 +1,47 @@
-import  React, { Component, useState } from "react";
+import  React, {useState } from "react";
 import '../styles/App.css';
 
-class App extends Component {
-    constructor() {
-        super();
-        this.state = { name1: "", name2: "", data: "" };
-    }
-    render() {
-        const onSubmit = (e) => {
-            e.preventDefault();
-            const name1 = e.target.name1.value;
-            const name2 = e.target.name2.value;
-            if(name1==="" || name2===""){
-                return;
-            }
-            let arr = [
-                "Siblings",
-                "Friends",
-                "Love",
-                "Affection",
-                "Marriage",
-                "Enemy",
-            ];
+const App = function(){
+    const [name1,setName1] = useState("");
+    const [name2,setName2] = useState("");
+    const [result,setResult] = useState("");
+    const relation = function(){
+        if(name1.length>3 && name2.length>3){
             let obj = {};
             let count = 0;
-            for (let i of name1) {
-                if (obj[i]) {
-                    obj[i]++;
-                } else {
-                    obj[i] = 1;
+            let array = ["Siblings","Friends","Love","Affection","Marriage","Enemy"]
+            for (let i=0;i<name1.length;i++){
+                if(obj[name1[i]]){
+                    obj[name1[i]]++;
+                }
+                else{
+                    obj[name1[i]]=1
                 }
             }
-            for (let i of name2) {
-                if (obj[i] && obj[i] > 0) {
+            for (let i=0;i<name2.length;i++){
+                if(obj[name2[i]] && obj[name2[i]]>0){
                     count++;
-                    obj[i]--;
+                    obj[name2[i]]--;
                 }
             }
-            const result = (name1.length + name2.length - count * 2) % 6;
-            this.state.data = arr[result];
-            this.setState({ ...this.state });
-        };
-        const clear = () => {
-            this.setState({
-                name1: (this.state.name1 = ""),
-                name2: (this.state.name2 = ""),
-                data: (this.state.data = ""),
-            });
-        };
-        return (
-            <div id="main">
-                {/* Do not remove the main div */}
-                <form onSubmit={onSubmit}>
-                    <input
-                        type="text"
-                        placeholder="Enter first name"
-                        name="name1"
-                        value={this.state.name1}
-                        data-testid="input1"
-                        onChange={(e) => {
-                            this.setState({ name1: (this.state.name1 = e.target.value) });
-                        }}
-                    />
-                    <input
-                        type="text"
-                        placeholder="Enter second name"
-                        name="name2"
-                        value={this.state.name2}
-                        data-testid="input2"
-                        onChange={(e) => {
-                            this.setState({ name2: (this.state.name2 = e.target.value) });
-                        }}
-                    />
-                    <button data-testid="calculate_relationship">
-                        Calculate Relationship Future
-                    </button>
-                    <button type="button" onClick={clear} data-testid="answer">
-                        Clear
-                    </button>
-                </form>
-                <h3>{this.state.data}</h3>
-            </div>
-        );
+            let len = ((name1.length+name2.length)-(count*2))%6;
+            setResult(array[len])
+        }
+        else{
+            alert("Name length should be greater than three")
+        }
     }
+    const clear = function(){
+        setName1("");
+        setName2("");
+    }
+    return <div className="conatiner">
+        <input type="text" value={name1} onChange={e=>setName1(e.target.value)}/>
+        <input type="text" value={name2} onChange={e=>setName2(e.target.value)}/>
+        <button onClick={relation}>Find Relation</button>
+        <button onClick={clear}>Clear</button>
+        <h1>{result}</h1>
+    </div>
 }
 
 
